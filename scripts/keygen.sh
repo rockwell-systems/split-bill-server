0279ec441dcdf3b5054e4aa2c9d65b03dae73d1a
KaungWai <kaungwai219@gmail.com>
2022-12-18 02:32:20 +0900

    keygen script added

---
+++ scripts/keygen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## Algorithm: RSA; Key length: 2048; Format: pem ##
+node -e "console.log('Generating key pairs for jwt signing and verification')"
+node -e "console.log('Algorithm: RSA; Key length: 2048; Format: pem')"
+
+# generate private key
+openssl genrsa -out ./keys/private 2048
+node -e "console.log('rsa pirvate key generted')"
+
+# genreate public key using private key
+openssl rsa -in ./keys/private -pubout -out ./keys/public.pub
+node -e "console.log('rsa public key generted')"
