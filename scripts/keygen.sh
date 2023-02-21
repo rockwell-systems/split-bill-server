cb5af249c6b50bed9420498abc04c564c034f67a
Kaung Wai <kaungwai219@gmail.com>
2023-02-22 02:14:18 +0900

    keygen.sh typo fixed

--- scripts/keygen.sh
+++ scripts/keygen.sh
@@ -6,7 +6,7 @@ node -e "console.log('Algorithm: RSA; Key length: 2048; Format: pem')"
 
 # generate private key
 openssl genrsa -out ./keys/private 2048
-node -e "console.log('rsa pirvate key generted')"
+node -e "console.log('rsa pirvate key generated')"
 
 # genreate public key using private key
 openssl rsa -in ./keys/private -pubout -out ./keys/public.pub
